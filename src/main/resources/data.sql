e91ed18654a328d4b38ea09f34f8883600c3ac5a
rdahane <60604855+raniadahane@users.noreply.github.com>
2023-12-13 19:02:09 +0100

    Add data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+DELETE FROM student;
+INSERT INTO student (id, firstName,lastName, age) VALUES (1, 'Alexis','NGUYEN', 22);
+INSERT INTO student (id, firstName,lastName, age) VALUES (2, 'Alex','braquo', 21);
+INSERT INTO student (id, firstName,lastName, age) VALUES (3, 'Jonathan','leloup', 45);
+INSERT INTO student (id, firstName,lastName, age) VALUES (4, 'Rania','Dahane', 60);
